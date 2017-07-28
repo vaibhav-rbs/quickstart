a96f12600174d8b29b8990e69e4a3d051eee6de2
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-27 22:05:32 -0700

    added ability to create a hero!!

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,5 +42,12 @@ import { Hero } from './hero';
             .then(() => hero)
             .catch(this.handleError);
         }
+        create(name: string): Promise <Hero> {
+            return this.http
+                .post(this.heroesUrl, JSON.stringify({name: name}), { headers: this.headers})
+                .toPromise()
+                .then(res => res.json().data as Hero)
+                .catch(this.handleError);
+        }
 
 }
