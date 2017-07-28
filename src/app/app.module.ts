0d0d0b132bd2ffe4eeeca37979bec02fbffba2bc
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-28 15:12:58 -0700

    tour of heroes completed, Final commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -25,7 +26,8 @@ import { HeroDetailComponent } from './hero-detail.component';
     AppComponent,
     DashboardComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroSearchComponent
      ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
